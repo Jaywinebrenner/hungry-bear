d9cce6c18730e50f46c5625b9cca7b5674f42124
Jay Winebrenner <jaywinebrenner@gmail.com>
2019-11-17 12:29:08 -0800

    set up and ready for the UI to be plumbed

--- src/main.js
+++ src/main.js
@@ -4,9 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { HungryBear } from './../src/hungrybear.js';
 
-$("#feed").click(function(){
-  let newBear = new HungryBear();
-   $("ul").append("<li>You fed the bear!</li>");
 
-  setHunger();
-})
+$(document).ready(function(){
+  $("#feed").click(function(){
+    let newBear = new HungryBear();
+    $("ul").append("<li>You fed the bear!</li>");
+
+    newBear.setHunger();
+    newBear.didYouGetEaten();
+    newBear.feed();
+
+
+  });
+});
